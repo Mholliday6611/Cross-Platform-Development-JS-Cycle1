01ef4032a1ffa6733edf2579369c44535ecf67b7
mholliday6611 <mholliday6611@gmail.com>
2017-05-11 20:52:36 -0400

    mongodbpractice

--- week5/server.js
+++ week5/server.js
@@ -4,12 +4,19 @@
 	var app = express();
 
 	app.get('/', function(request, respond){
-		respond.json({
+		if(process.env.NODE_ENV == 'dev'){
+			respond.json({
 			"user": process.env.DB_USER,
 			"pass": process.env.DB_PASS
 		});
+		}else {
+			respond.json({
+				"user": "User does not exist",
+				"pass": "user does not exist"
+			});
+		}
 	});
 
-	app.listen(8080);
+	app.listen(process.env.PORT || 8080);
 
 	console.log("Server is Running!?!");
